9ef6328945e2e45f7dc19c4a267d37fca1cb0b21
UFUOMA OGEDEGBE <ufuogbe@gmail.com>
2025-11-30 05:44:38 +0000

    orders/api/views.py modified

--- orders/api/views.py
+++ orders/api/views.py
@@ -6,6 +6,8 @@ from .serializers import OrderSerializer
 class OrderViewSet(viewsets.ModelViewSet):
     serializer_class = OrderSerializer
     permission_classes = [permissions.IsAuthenticated]
+    lookup_field = "id"
+    lookup_url_kwarg = "id"
     filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     filterset_fields = ['status', 'transport']
     search_fields = ['first_name', 'last_name', 'email']
@@ -13,7 +15,14 @@ class OrderViewSet(viewsets.ModelViewSet):
     ordering = ['-created']
 
     def get_queryset(self):
-        return Order.objects.filter(user=self.request.user)
+        # Prevent swagger schema error
+        if getattr(self, "swagger_fake_view", False):
+            return Order.objects.none()
+
+        user = self.request.user
+        if user.is_authenticated:
+            return Order.objects.filter(user=user)
+        return Order.objects.none()
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
